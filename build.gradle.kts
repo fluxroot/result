e40505db01b8b1eb34cecd10ce7d0f08437a2e1a
Phokham Nonava <pn@nonava.com>
2022-03-10 00:48:23 +0100

    Add basic build

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	`java-library`
+	checkstyle
+	pmd
+	alias(libs.plugins.spotbugs)
+}
+
+group = "dev.nonava.result"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	testImplementation(libs.junit)
+}
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(8))
+	}
+	withSourcesJar()
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
+
+checkstyle {
+	config = resources.text.fromFile("$rootDir/config/checkstyle/checkstyle.xml")
+	configDirectory.set(file("$rootDir/config/checkstyle"))
+}
+
+pmd {
+	ruleSets = listOf()
+	ruleSetConfig = resources.text.fromFile("$rootDir/config/pmd/main-ruleset.xml")
+}
+
+tasks.pmdTest {
+	ruleSets = listOf()
+	ruleSetConfig = resources.text.fromFile("$rootDir/config/pmd/test-ruleset.xml")
+}
---
+++ src/main/java/dev/nonava/result/Result.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 Phokham Nonava
+ *
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package dev.nonava.result;
+
+public final class Result {
+}
